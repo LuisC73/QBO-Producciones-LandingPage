3096dca15247571a825867a02ab75d7c075d502e
Luis Miguel Castro Curequia <luiscastroc11@gmail.com>
2023-06-06 21:29:06 -0500

    Se cambia path de fonts

--- src/App.svelte
+++ src/App.svelte
@@ -26,12 +26,12 @@
 
 <style>
   @font-face {
-    src: url("public/font/Futura\ Book\ font.ttf");
+    src: url("/font/Futura\ Book\ font.ttf");
     font-family: "Futura Light";
   }
 
   @font-face {
-    src: url("public/font/Futura\ Heavy\ font.ttf");
+    src: url("/font/Futura\ Heavy\ font.ttf");
     font-family: "Futura Bold";
   }
 
